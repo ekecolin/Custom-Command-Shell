853a9499e61b4018659ce5c900e60e3299e480f7
ekecolin <117390062+ekecolin@users.noreply.github.com>
2022-11-16 00:04:37 +0000

    Create utility.c

---
+++ src/utility.c
@@ -0,0 +1,19 @@
+/*Author: Colin Ekedigwe - Student Number: 20403046 - I am aware of the Universities regards to breaches of academic
+integrity and plagiarism as serious. Any assistance I have gotten by using code to help me has been referenced and 
+stated with links. I am writing this to acknowledge that a breach of the rules may result in penalties being imposed.*/
+
+// Needed to put this here in order for my make file to run
+
+void intro() {
+sleep(1);
+    printf("*******************************************************");
+    printf("\n\n\n\t******COLIN'S SHELL - 20403046******");
+    printf("\n\n\t--------USE AT YOUR OWN RISK--------");
+    printf("\n\n------Unsure of what commands to use? Type 'help'------");
+    printf("\n\n\n*******************************************************\n");
+    sleep(1.2);
+    printf("Welcome User to Colin's custom C command shell program!\n");
+    printf("-------------------------------------------------------\n");
+    sleep(1);
+    printf("\nLet's begin: ");
+}
